1ec27a022798a6089082a674949ff748d74c5955
tharulYT <81139511+tharulYT@users.noreply.github.com>
2021-08-27 08:53:42 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+music.playMelody("C5 G B D A F C E ", 120)
+basic.showIcon(IconNames.Skull)
+basic.showIcon(IconNames.EigthNote)
+basic.showIcon(IconNames.Asleep)
+basic.showLeds(`
+    # # . # #
+    # # . # #
+    . . # . .
+    # . . . #
+    # # # # #
+    )
